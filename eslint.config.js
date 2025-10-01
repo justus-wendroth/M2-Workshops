3091a7d7f61093fb4f895e1bab7af0f93801b137
Justus Wendroth <justus.wendroth@outlook.de>
2025-10-01 16:53:40 +0200

    Added eslint and formatted files

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+const js = require('@eslint/js');
+
+module.exports = [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'commonjs',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                Buffer: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                module: 'readonly',
+                require: 'readonly',
+                exports: 'readonly',
+                global: 'readonly',
+                setTimeout: 'readonly',
+                clearTimeout: 'readonly',
+                setInterval: 'readonly',
+                clearInterval: 'readonly'
+            }
+        },
+        rules: {
+            'indent': ['error', 2],
+        },
+        files: ['js/**/*.js'],
+        ignores: [
+            'node_modules/**',
+            'css/**',
+            'package-lock.json'
+        ]
+    }
+];
--- js/inMemoryWorkshop.js
+++ js/inMemoryWorkshop.js
@@ -1,58 +1,58 @@
 let inMemoryWorkshop;
 
 function init() {
-    inMemoryWorkshop = [];
-    return Promise.resolve();
+  inMemoryWorkshop = [];
+  return Promise.resolve();
 }
 
 function getWorkshopList() {
-    return new Promise((resolve, ) => {
-        resolve(inMemoryWorkshop)
-    })
+  return new Promise((resolve) => {
+    resolve(inMemoryWorkshop);
+  });
 }
 
 function getWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("name parameter is required"))
-        }
-        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
-    })
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error("name parameter is required"));
+    }
+    resolve(inMemoryWorkshop.find((workshop) => workshop.name === workshop));
+  });
 }
 
 function addWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("Workshop name required"))
-        }
-        if (!description) {
-            reject(new Error("Workshop description required"))
-        }
-        inMemoryWorkshop.push({
-            name,
-            description
-        })
-        resolve()
-    })
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error("Workshop name required"));
+    }
+    if (!description) {
+      reject(new Error("Workshop description required"));
+    }
+    inMemoryWorkshop.push({
+      name,
+      description,
+    });
+    resolve();
+  });
 }
 
 function removeWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+  return new Promise((resolve, reject) => {
+    reject(new Error("Not implemented"));
+  });
 }
 
 function updateWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+  return new Promise((resolve, reject) => {
+    reject(new Error("Not implemented"));
+  });
 }
 
 module.exports = {
-    init,
-    getWorkshopList,
-    getWorkshopByName,
-    addWorkshop,
-    removeWorkshopByName,
-    updateWorkshop
-}
+  init,
+  getWorkshopList,
+  getWorkshopByName,
+  addWorkshop,
+  removeWorkshopByName,
+  updateWorkshop,
+};
